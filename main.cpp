24ed000a4bea8f47bb0fa48a7a11180397f3f2c4
شیدا زارع <168183545+Sheydazare@users.noreply.github.com>
2024-06-06 10:19:15 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,63 +1,34 @@
 
+
 #include <iostream>
 using namespace std;
-int pow(int a,int b){
-    if(b==0){
-        return 1;
+int main()
+{
+    
+    int a[9]={0},n;
+    do{
+        cout<<"enter a number";
+        cin>>n;
+        if(n>0&&n<10){
+            a[n-1]++;
+        }
+    }while(n!=0);
+    int indexmax=0,indexmin=0,max=a[0],min=a[0];
+    for(int i=0;i<9;i++){
+        cout<<i+1<<"i"<<a[i]<<endl;
+    
+    if(a[i]>max){
+        indexmax=i;
+        max=a[i];
     }
-    return a*pow(a,b-1);
-}
-int fibo(int n){
-    if(n==1||n==2){
-        return 1;
+    if(a[i]<min){
+        indexmin=i;
+        min=a[i];
     }
-    return fibo(n-2)+fibo(n-1);
 }
-int zarb(int a,int b){
-    if(b==0){
-        return 0;
-    }
-    return a+zarb(a,b-1);
-}
-int fact(int n){
-    if(n==1){
-        return 1;
-    }
-    return n*fact(n-1);
-}
-struct calculator{
-    int lv,rv;
-    char op;
-void run()
-    {
-        cout<<"enter operator"<<endl;
-        cin>>op;
-        switch(op){
+    cout<<"max :"<<indexmax+1<<":"<<a[indexmax]<<endl;
+     
+    cout<<"mix :"<<indexmin+1<<":"<<a[indexmin]<<endl;
 
-        case'^':
-           cin>>lv>>rv;  
-           cout<<pow(lv,rv);
-           break;
-        case'f':
-           cin>>lv;  
-           cout<<fibo(lv);
-           break;
-        case'*':
-           cin>>lv>>rv;  
-           cout<<zarb(lv,rv);
-           break;
-        case'!':
-           cin>>lv;  
-           cout<<fact(lv);
-           break;
-        default:
-        cout<<"Bad!";
-        }
-    }
-                };    
-        int main()
-        {
-            calculator c;
-            c.run();
-            return 0;
-        }
+    return 0;
+}
